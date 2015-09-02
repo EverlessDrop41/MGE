395ee5c55caf23e2600d0742bd7f86e95dace88d
EverlessDrop41 <tomfperegrine@googlemail.com>
2015-09-02 12:51:50 +0100

    Began Camera Example level

---
+++ MGE/Framework/Examples/CameraExample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MGE.Framework.Examples
+{
+    class CameraExample : Level
+    {
+        public Camera cam;
+
+        public CameraExample (MGEWindow win) : base (win)
+        {
+            cam = new Camera(win);
+        }
+    }
+}
